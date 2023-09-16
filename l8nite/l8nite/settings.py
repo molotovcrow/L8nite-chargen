abbda20f1e2bd66000252e7bf802940a3d36e777
molotovcrow <camo10293@gmail.com>
2023-09-16 18:30:31 -0400

    L8nite DB nonsetup filestructure

--- l8nite/l8nite/settings.py
+++ l8nite/l8nite/settings.py
@@ -74,11 +74,15 @@ WSGI_APPLICATION = 'l8nite.wsgi.application'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "OPTIONS": {
+            "service": "my_service",
+            "passfile": ".my_pgpass",
+        },
     }
 }
+
 
 
 # Password validation
