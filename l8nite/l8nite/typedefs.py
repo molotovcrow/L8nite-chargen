5f8daa2d0889709d95679c8293b1a0728b6b9663
Emily Vogel <32078831+EVogel1999@users.noreply.github.com>
2024-03-09 11:21:32 -0500

    Use onetoone fields instead

--- l8nite/l8nite/typedefs.py
+++ l8nite/l8nite/typedefs.py
@@ -1,3 +1,6 @@
+FEET_TO_METERS = 3.281
+""" How many feet there are in 1 meter. """
+
 DIE_TYPES = (
     'D4',
     'D6',
@@ -6,5 +9,4 @@ DIE_TYPES = (
     'D12',
     'D20',
 )
-
 DIE_TYPE_CHOICES = list((die_type, die_type) for die_type in DIE_TYPES)
