9512f650c7559460b3ffdef41645e3d22056c8c5
Emily Vogel <32078831+EVogel1999@users.noreply.github.com>
2024-03-09 10:35:31 -0500

    Remove restart/sync

--- scripts/start-app.sh
+++ scripts/start-app.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+# TODO: Make makemigrations/migrate optional (maybe ENV?)
+
 python l8nite/manage.py makemigrations
 python l8nite/manage.py migrate
 python l8nite/manage.py runserver 0.0.0.0:${DJANGO_PORT}
