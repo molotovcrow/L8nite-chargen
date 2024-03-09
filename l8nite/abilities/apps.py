6d6db5e73ae2b0712df6bf185cd8b0e6f94e5549
Andy <andrew.stoerkel@gmail.com>
2024-03-09 09:59:20 -0500

    added character stuff

---
+++ l8nite/abilities/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class AbilitiesConfig(AppConfig):
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "abilities"
---
+++ l8nite/l8nite/typedefs.py
@@ -0,0 +1,13 @@
+RACE_CHOICES = (
+    ("HU", "Human"),
+    ("OR", "Orc"),
+    ("AK", "Akuak"),
+    ("AI", "Artificial Intelligence"),
+    ("AS", "Asheni"),
+    ("BA", "Bahah"),
+    ("DW", "Dwarf"),
+    ("GO", "Goblin"),
+    ("TR", "Troll"),
+    ("IX", "Ixia"),
+    ("QI", "Qitop"),
+)
